95d8bd423b8ca13015c9e8484d3d33117f10ec1f
VAIBHAV189 <varshney.vaibhav18@gmail.com>
2021-08-15 10:48:33 +0530

    Combining Requests for creator

--- routes/creator.js
+++ routes/creator.js
@@ -3,15 +3,46 @@ const requests =    require('../schema/requestList.js').reqList
 console.log(requests)
 
 route.get('/',(req,res)=>{
-    res.render('../public/creator/index.hbs');
+
+    let pendingReqList=[]
+    let paidReqList=[]
+    let compReqList=[]
+    requests.find(
+    {
+        creatorUsername: "vagi",
+        status: "Pending"
+    }
+    ).then((pending)=>{
+
+        pendingReqList=pending
+    })
+    request.find(
+    {
+        creatorUsername : "vagi",
+        status : "Completed"
+    }
+    ).then((completed)=>{
+        compReqList=completed
+    })
+
+    request.find(
+    {
+        creatorUsername : "vagi",
+        status : "Paid"
+    }
+    ).then((paid)=>{
+        paidReqList=paid
+    })
+
+    res.render('../public/creator/index.hbs',{pendingReqList,paidReqList,compReqList});
 })
 route.get('/pendingRequests', (req, res)=>{
     // console.log(req.userObject)
     requests.find(
-        {
-            creatorUsername: "vagi",
-            status: "Pending"
-        }
+    {
+        creatorUsername: "vagi",
+        status: "Pending"
+    }
     ).then((pendingReqList)=>{
         // console.log(pendingReqList)
         res.render('../public/creator/index.hbs',{pendingReqList})
@@ -20,8 +51,10 @@ route.get('/pendingRequests', (req, res)=>{
 
 route.get('/paidRequests', (req, res)=>{
     request.find(
-        {creatorUsername : req.user.username},
-        {status : "Paid"}
+        {
+            creatorUsername : req.user.username,
+            status : "Paid"
+        }
     ).then((paidReqList)=>{
         res.send(paidReqList)
     })
@@ -29,8 +62,10 @@ route.get('/paidRequests', (req, res)=>{
 
 route.get('/completedRequests', (req, res)=>{
     request.find(
-        {creatorUsername : req.user.username},
-        {status : "Completed"}
+        {
+            creatorUsername : req.user.username,
+            status : "Completed"
+        }
     ).then((compReqList)=>{
         res.send(compReqList)
     })
