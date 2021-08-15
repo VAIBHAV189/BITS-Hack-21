ae6dc239d9051df79ecce3dc593f62f6e8682f16
achint740 <achintdawra740@gmail.com>
2021-08-15 15:01:12 +0530

    UI Changes II

--- public/creator/script.js
+++ public/creator/script.js
@@ -116,3 +116,24 @@ $('#fetchAdSense').on('click',()=>{
     })
 })
 
+
+
+// $("#logout").hide();
+//     $.get('/users/profile',(data)=>{
+//         if(data.username==undefined){
+//             alert("Please Login");
+//             document.location.href='/login';
+//         }
+//         else if(data.category!="admin"){
+//             alert("Not Authorized");
+//             document.location.href = '/';
+//         }
+//         else{
+//             console.log("Welcome " + data.username);
+//             $('#login123')
+//                 .text(data.username)
+//                 .attr("href","#")
+//             $("#logout").show();
+//             viewusers();
+//         }
+//     });
